7f7577d206762e0eaeb349a57681d41a3b0d60c1
sangminlee98 <tkdals0975@naver.com>
2022-06-17 22:33:08 +0900

    slider animation

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import styled from 'styled-components';
 import { getMovies, INowPlaying } from '../api';
@@ -33,7 +34,38 @@ const Overview = styled.p`
   width: 50%;
 `;
 
+const Slider = styled(motion.div)`
+  position: relative;
+  top: -100px;
+`;
+const Row = styled(motion.div)`
+  position: absolute;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-gap: 10px;
+  width: 100%;
+`;
+const Box = styled(motion.div)`
+  height: 200px;
+  background-color: white;
+  color: black;
+`;
+
+const rowVariants = {
+  hidden: {
+    x: window.outerWidth + 10
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.outerWidth - 10
+  },
+};
+
 const Home = () => {
+  const [index, setIndex] = useState(0);
+  const increaseIndex = () => setIndex(prev => prev+1);
   const {data, isLoading} = useQuery<INowPlaying>(['movies','nowPlaying'], getMovies);
   console.log(data, isLoading);
   return (
@@ -42,10 +74,19 @@ const Home = () => {
         <Loader>Loading...</Loader> 
       ) : (
         <>
-          <Banner bgPath={makeImagePath(data?.results[0].backdrop_path || '')}>
+          <Banner onClick={increaseIndex} bgPath={makeImagePath(data?.results[0].backdrop_path || '')}>
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
+          <Slider>
+            <AnimatePresence>
+              <Row variants={rowVariants} initial='hidden' animate='visible' exit='exit' transition={{type:'tween', duration: .8}} key={index}>
+                {
+                  [1,2,3,4,5,6].map(i => <Box key={i}>{i}</Box>)
+                }
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
